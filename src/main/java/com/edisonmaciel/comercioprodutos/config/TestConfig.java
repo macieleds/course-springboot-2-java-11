af4fbe6a42c16c5c6e079c8a46108490b1423442
Edison Maciel <maciel.eds@gmail.com>
2020-05-18 19:09:48 -0300

    Payment, one-to-one association

--- src/main/java/com/edisonmaciel/comercioprodutos/config/TestConfig.java
+++ src/main/java/com/edisonmaciel/comercioprodutos/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.edisonmaciel.comercioprodutos.entities.Category;
 import com.edisonmaciel.comercioprodutos.entities.Order;
 import com.edisonmaciel.comercioprodutos.entities.OrderItem;
+import com.edisonmaciel.comercioprodutos.entities.Payment;
 import com.edisonmaciel.comercioprodutos.entities.Product;
 import com.edisonmaciel.comercioprodutos.entities.User;
 import com.edisonmaciel.comercioprodutos.entities.enums.OrderStatus;
@@ -82,6 +83,12 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 		}
 
 }
